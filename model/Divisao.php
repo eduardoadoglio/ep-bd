913a788e26f62830ad13e1058fe9b8808b243937
eduardoadoglio <eduardoadoglio4@gmail.com>
2020-12-14 23:50:41 -0300

    Add divisao creation

--- model/Divisao.php
+++ model/Divisao.php
@@ -2,7 +2,7 @@
 
 require_once('Database.php');
 
-public class Divisao {
+class Divisao {
 
     private $database;
 
@@ -10,8 +10,15 @@ public class Divisao {
         $this->database = new Database();
     }
     
-    function createDivisao($name){
-
+    function createDivisao($id, $barcos, $avioes, $tanques, $homens){
+        $this->database->executeQuery("INSERT INTO divisao (codigog, numbaixasd, barcos, avioes, tanques, homens) VALUES (:codigog, 0, :barcos, :avioes, :tanques, :homens)", array(
+            ":codigog" => $id,
+            ":barcos" => $barcos,
+            ":avioes" => $avioes,
+            ":tanques" => $tanques,
+            ":homens" => $homens
+        ));
+        return $this->database->getLastInsertId();
     }
 
     function updateDivisao($name){
@@ -22,8 +29,12 @@ public class Divisao {
 
     }
 
-    function getDivisaoByName($name){
-
+    function getDivisaoById($codigog, $id){
+        $response = $this->database->executeQuery("SELECT * FROM divisao WHERE codigog = :codigog AND nrodivisao = :id", array(
+            ":codigog" => $codigog,
+            ":id" => $id
+        ));
+        return $response->fetch(PDO::FETCH_ASSOC);
     }
 
     function getAllDivisoes($name){
