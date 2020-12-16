784eb7c014f18e1ae3509f7e3e592a8321498204
eduardoadoglio <eduardoadoglio4@gmail.com>
2020-12-16 18:40:11 -0300

    Add lider politico creation design

--- view/liderPolitico/createLiderPolitico.php
+++ view/liderPolitico/createLiderPolitico.php
@@ -1,30 +1,38 @@
 <?php
-include("../../template/header.php");
+    include("../../template/header.php");
+    include("../../template/menu.php");    
+    require_once('../../model/GrupoMilitar.php');
 
-require_once('../../model/GrupoMilitar.php');
+    $grupoMilitarObj = new GrupoMilitar();
 
-$grupoMilitarObj = new GrupoMilitar();
-
-$gruposMilitares = $grupoMilitarObj->getAllGruposMilitares();
+    $gruposMilitares = $grupoMilitarObj->getAllGruposMilitares();
 
 ?>
 <body>
-    <form action="../../action/liderPolitico/createLiderPolitico.php" method="POST">
-        Nome: <br>
-        <input type="text" name="liderPoliticoName" required> <br>
-        Apoios: <br>
-        <input type="text" name="liderPoliticoApoios" required> <br>
-        Grupo que lidera: <br>
-        <select name="grupoMilitarId"> 
-        <option value="" selected>Selecione um grupo militar</option>
-            <?php
-                foreach($gruposMilitares as $grupo){
-                    echo '<option value="'.$grupo['codigog'].'">'.$grupo['nomegrupo'].'</option>';
-                }
-            ?>
-        </select> <br> 
-        <input type="submit" value="Enviar">
-    </form>
+    <div class="container-fluid full-page-form">
+        <div class="row justify-content-center">
+            <div class="col-md-12 form">
+                <div class="form-content">
+                    <h1>Criação de Líder político</h1>
+                    <form action="../../action/liderPolitico/createLiderPolitico.php" method="POST">
+                        Nome: <br>
+                        <input type="text" name="liderPoliticoName" required> <br>
+                        Apoios: <br>
+                        <input type="text" name="liderPoliticoApoios" required> <br>
+                        Grupo que lidera: <br>
+                        <select name="grupoMilitarId"> 
+                        <option value="" selected>Selecione um grupo militar</option>
+                            <?php
+                                foreach($gruposMilitares as $grupo){
+                                    echo '<option value="'.$grupo['codigog'].'">'.$grupo['nomegrupo'].'</option>';
+                                }
+                            ?>
+                        </select> <br> 
+                        <input type="submit" value="Enviar">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
-<?php include("../../template/footer.php"); ?>
 </html>
