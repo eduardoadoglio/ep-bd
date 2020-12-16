f86a3d438c948f5d876c17a670cb53cdb01a5476
eduardoadoglio <eduardoadoglio4@gmail.com>
2020-12-16 15:28:28 -0300

    Setup home base

--- index.php
+++ index.php
@@ -1,12 +1,162 @@
 <html>
-<head>
-    <title>Trabalho de BD</title>
-</head>
+<?php
+    include("template/header.php");
+    include("template/menu.php");
+?>
 <body>
-    <a href="/view/conflito/conflitosBelicos.php">Conflitos Bélicos</a>
-    <a href="/view/grupoMilitar/grupoMilitar.php">Grupos militares</a>
-    <a href="/view/divisao/divisao.php">Divisões</a>
-    <a href="/view/chefeMilitar/chefeMilitar.php">Chefe militar</a>
-    <a href="/view/liderPolitico/liderPolitico.php">Líder político</a>
+<div class="home-hero">
+    <div class="hero-text">
+        <h1>Conflitos Bélicos</h1>
+        <p>subtitulo de impacto</p>    
+    </div>
+    <button><a href="#main-content" >V</a></button>
+</div>
+<div class="container-fluid" id="main-content">
+    <section class="services">
+        <h2>Cadastros</h2>
+        <div class="row justify-content-center services-box">
+            <div class="service col-md-4" id="services">
+                <img src="/assets/image/conflitoBelicoIcon.png">
+                <p>
+                    Conflitos belicos sei o que sei que la
+                </p>
+                <button>vai la irmao</button>
+            </div>
+            <div class="service col-md-4">
+                <img src="/assets/image/conflitoBelicoIcon.png">
+                <p>
+                    Conflitos belicos sei o que sei que la
+                </p>
+                <button>vai la irmao</button>
+            </div>
+            <div class="service col-md-4">
+                <img src="/assets/image/conflitoBelicoIcon.png">
+                <p>
+                    Conflitos belicos sei o que sei que la
+                </p>
+                <button>vai la irmao</button>
+            </div>
+        </div>
+        <div class="row justify-content-center services-box">
+            <div class="service col-md-4">
+                <img src="/assets/image/conflitoBelicoIcon.png">
+                <p>
+                    Conflitos belicos sei o que sei que la
+                </p>
+                <button>vai la irmao</button>
+            </div>
+            <div class="service col-md-4">
+                <img src="/assets/image/conflitoBelicoIcon.png">
+                <p>
+                    Conflitos belicos sei o que sei que la
+                </p>
+                <button>vai la irmao</button>
+            </div>
+        </div>
+        <h2>Buscas</h2>
+        <div class="row content-left">
+            <div class="content-text col-md-8">
+            <h3>Listar os traficantes e os grupos armados (Nome) para os quais os traficantes fornecem armas “Barret M82” ou “M200 intervention”</h3>
+
+            Saepe temporibus placeat atque perspiciatis doloremque autem enim ducimus. Molestiae neque totam esse ad dolores quo. Natus veritatis dignissimos non ut ipsa. Id modi necessitatibus magni nobis. At voluptas dolor quam dolorem quo itaque facere enim. Repudiandae vel dignissimos sunt voluptas nam incidunt.
+
+            Dolorum et culpa qui. Vitae id et consequatur aliquam. Qui recusandae hic quos.
+
+            Sunt aut nisi aliquam. Reprehenderit omnis et quia. Earum et magnam sit dolores minima ea dolores unde. Quia sit ad architecto. Repudiandae reprehenderit saepe quam quam veniam natus in. Excepturi consequatur quia laborum quia et numquam id.
+
+            Iure consectetur quam dolor voluptatem officia ab veniam perspiciatis. Sit omnis voluptatem voluptatem dicta pariatur possimus. Ut voluptates at amet.
+            </div>
+            <div class="content-image col-md-4">
+                imaje   
+            </div>
+        </div>
+    </section>
+    <section class="content-section">
+        <div class="row content-right">
+            <div class="content-image col-md-4">
+                imaje   
+            </div>
+            <div class="content-text col-md-8">
+            <h3>Listar os 5 maiores conflitos em número de mortos</h3>
+
+            Saepe temporibus placeat atque perspiciatis doloremque autem enim ducimus. Molestiae neque totam esse ad dolores quo. Natus veritatis dignissimos non ut ipsa. Id modi necessitatibus magni nobis. At voluptas dolor quam dolorem quo itaque facere enim. Repudiandae vel dignissimos sunt voluptas nam incidunt.
+
+            Dolorum et culpa qui. Vitae id et consequatur aliquam. Qui recusandae hic quos.
+
+            Sunt aut nisi aliquam. Reprehenderit omnis et quia. Earum et magnam sit dolores minima ea dolores unde. Quia sit ad architecto. Repudiandae reprehenderit saepe quam quam veniam natus in. Excepturi consequatur quia laborum quia et numquam id.
+
+            Iure consectetur quam dolor voluptatem officia ab veniam perspiciatis. Sit omnis voluptatem voluptatem dicta pariatur possimus. Ut voluptates at amet.
+            </div>
+        </div>
+        <div class="row content-left">
+            <div class="content-text col-md-8">
+            <h3>Listar as 5 maiores organizações em número de mediações</h3>
+
+            Saepe temporibus placeat atque perspiciatis doloremque autem enim ducimus. Molestiae neque totam esse ad dolores quo. Natus veritatis dignissimos non ut ipsa. Id modi necessitatibus magni nobis. At voluptas dolor quam dolorem quo itaque facere enim. Repudiandae vel dignissimos sunt voluptas nam incidunt.
+
+            Dolorum et culpa qui. Vitae id et consequatur aliquam. Qui recusandae hic quos.
+
+            Sunt aut nisi aliquam. Reprehenderit omnis et quia. Earum et magnam sit dolores minima ea dolores unde. Quia sit ad architecto. Repudiandae reprehenderit saepe quam quam veniam natus in. Excepturi consequatur quia laborum quia et numquam id.
+
+            Iure consectetur quam dolor voluptatem officia ab veniam perspiciatis. Sit omnis voluptatem voluptatem dicta pariatur possimus. Ut voluptates at amet.
+            </div>
+            <div class="content-image col-md-4">
+                imaje   
+            </div>
+        </div>
+        <div class="row content-right">
+            <div class="content-image col-md-4">
+                imaje   
+            </div>
+            <div class="content-text col-md-8">
+            <h3>Listar os 5 maiores grupos armados com maior número de armas fornecidos.</h3>
+
+            Saepe temporibus placeat atque perspiciatis doloremque autem enim ducimus. Molestiae neque totam esse ad dolores quo. Natus veritatis dignissimos non ut ipsa. Id modi necessitatibus magni nobis. At voluptas dolor quam dolorem quo itaque facere enim. Repudiandae vel dignissimos sunt voluptas nam incidunt.
+
+            Dolorum et culpa qui. Vitae id et consequatur aliquam. Qui recusandae hic quos.
+
+            Sunt aut nisi aliquam. Reprehenderit omnis et quia. Earum et magnam sit dolores minima ea dolores unde. Quia sit ad architecto. Repudiandae reprehenderit saepe quam quam veniam natus in. Excepturi consequatur quia laborum quia et numquam id.
+
+            Iure consectetur quam dolor voluptatem officia ab veniam perspiciatis. Sit omnis voluptatem voluptatem dicta pariatur possimus. Ut voluptates at amet.
+            </div>
+        </div>
+        <div class="row content-left">
+            <div class="content-text col-md-8">
+            <h3>Listar o país e número de conflitos com maior número de conflitos religiosos.</h3>
+
+            Saepe temporibus placeat atque perspiciatis doloremque autem enim ducimus. Molestiae neque totam esse ad dolores quo. Natus veritatis dignissimos non ut ipsa. Id modi necessitatibus magni nobis. At voluptas dolor quam dolorem quo itaque facere enim. Repudiandae vel dignissimos sunt voluptas nam incidunt.
+
+            Dolorum et culpa qui. Vitae id et consequatur aliquam. Qui recusandae hic quos.
+
+            Sunt aut nisi aliquam. Reprehenderit omnis et quia. Earum et magnam sit dolores minima ea dolores unde. Quia sit ad architecto. Repudiandae reprehenderit saepe quam quam veniam natus in. Excepturi consequatur quia laborum quia et numquam id.
+
+            Iure consectetur quam dolor voluptatem officia ab veniam perspiciatis. Sit omnis voluptatem voluptatem dicta pariatur possimus. Ut voluptates at amet.
+            </div>
+            <div class="content-image col-md-4">
+                imaje   
+            </div>
+        </div>
+        <div class="row content-right">
+            <div class="content-image col-md-4">
+                imaje   
+            </div>
+            <div class="content-text col-md-8">
+            <h3>Listar os 5 maiores conflitos em número de mortos</h3>
+
+            Saepe temporibus placeat atque perspiciatis doloremque autem enim ducimus. Molestiae neque totam esse ad dolores quo. Natus veritatis dignissimos non ut ipsa. Id modi necessitatibus magni nobis. At voluptas dolor quam dolorem quo itaque facere enim. Repudiandae vel dignissimos sunt voluptas nam incidunt.
+
+            Dolorum et culpa qui. Vitae id et consequatur aliquam. Qui recusandae hic quos.
+
+            Sunt aut nisi aliquam. Reprehenderit omnis et quia. Earum et magnam sit dolores minima ea dolores unde. Quia sit ad architecto. Repudiandae reprehenderit saepe quam quam veniam natus in. Excepturi consequatur quia laborum quia et numquam id.
+
+            Iure consectetur quam dolor voluptatem officia ab veniam perspiciatis. Sit omnis voluptatem voluptatem dicta pariatur possimus. Ut voluptates at amet.
+            </div>
+        </div>
+    </section>
+    
+</div>
 </body>
+<?php
+    include("template/footer.php");
+?>
 </html>
