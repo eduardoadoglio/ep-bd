a36257d2b75dc1319db075f969e97d98d51d1942
eduardoadoglio <eduardoadoglio4@gmail.com>
2020-12-16 19:24:48 -0300

    Finish listing divisoes on divisao view

--- view/divisao/divisao.php
+++ view/divisao/divisao.php
@@ -4,14 +4,15 @@
     require_once("../../model/Divisao.php");
     $divisao = new Divisao();
     $divisoes = $divisao->getAllDivisoes();
-    // var_dump($divisoes);
-    // die;
 ?>
 <body>
+    <?php
+        if(count($divisoes) > 0):
+    ?>
     <div class="container-fluid table-content">
         <div class="row">
             <div class="col-md-12">
-                <h1>Divisão criada</h1>
+                <h1>Todas as divisões</h1>
                 <table class="table-data">
                     <thead>
                         <tr>
@@ -37,7 +38,22 @@
                 </table>    
             </div>
         </div>
-        
     </div>
+    <?php
+        else:
+    ?>
+    <div class="container-fluid table-content">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <h1>Não há nenhuma divisão criada</h1>
+                    <a href="createDivisao.php">
+                        <button class="default-button">Criar uma?</button>
+                    </a>  
+            </div>
+        </div>
+    </div>
+    <?php
+        endif;
+    ?>
 </body>
 </html>
