979b8cbe21c37cdeddb7e86fa59978cff3a9a16e
eduardoadoglio <eduardoadoglio4@gmail.com>
2020-12-18 19:31:21 -0300

    Change favicon

--- template/header.php
+++ template/header.php
@@ -3,6 +3,7 @@
 <head>
     <title>Trabalho de BD</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="shortcut icon" type="image/jpg" href="/assets/image/trabalhoLogo.png"/>
     <link rel="stylesheet" href="/assets/css/style.css">
     <script src="/assets/js/jquery-3.5.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
